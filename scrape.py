fd483da9bbd2f300d313e201be7bfa7758c62a93
Nikhil Nathwani <njn27@cornell.edu>
2016-07-19 12:58:38 -0400

    Completed method for turning kibana columns into JSON

--- scrape.py
+++ scrape.py
@@ -22,20 +22,37 @@ def getUserInput():
 def formatQueryURL(query,startDate="",endDate=""):
 	urlBase= 'http://ofeedbackdashboard.cloudapp.net/?#/discover?_g=(refreshInterval:(display:Off,pause:!f,section:0,value:0),'
 	time= 'time:(from:\''+startDate+'T04:00:00.000Z\',mode:absolute,to:\''+endDate+'T03:59:59.999Z\'))'
-	urlMid= '&_a=(columns:!(comment),index:feedback,interval:auto,query:(query_string:(analyze_wildcard:!t,'
+	urlMid= '&_a=(columns:!(comment,product,officeBuild,commentURL),index:feedback,interval:auto,query:(query_string:(analyze_wildcard:!t,'
 	q= 'query:\'(comment:%22'+query.replace(' ','%20')+'%22)%20AND%20(product:%22desktop%20word%22%20OR%20%22desktop%20powerpoint%22%20OR%20%22desktop%20excel%22)\')),'
 	urlEnd= 'sort:!(submitDate,desc))'
 	return urlBase+time+urlMid+q+urlEnd
 
 
-def getCommentColumn(soup):
+'''def getCommentColumn(soup):
 	body= soup.find("table",{"class" : "kbn-table"}).find("tbody")
 	rows= body.findAll('tr', {"class" : "discover-table-row"})
 	comments= []
 	for row in rows:
 		c= row.findAll('td')[-1].text
 		comments.append(c)
-	return column
+	return column'''
+
+
+def columnsToJSON(soup):
+	body= soup.find("table",{"class" : "kbn-table"}).find("tbody")
+	rows= body.findAll('tr', {"class" : "discover-table-row"})
+	feedback= []
+	#column order: time, comment, product, build, ofeedback url
+	colTitles= ["time", "comment", "product", "build", "url"]
+	for row in rows:
+		feedback.append({})
+		cols= row.findAll('td')
+		for i,col in enumerate(cols[1:]):
+			feedback[-1][colTitles[i]]= col.text.encode("ascii","ignore")
+		#print feedback[-1]
+	return feedback
+		
+
 
 
 
@@ -50,13 +67,13 @@ if __name__ == "__main__":
 	t= time.time()
 
 	driver.get(formatQueryURL("Save as",'2016-07-15','2016-07-20'))
-	element = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CLASS_NAME, "vis-tooltip")))	
+	element = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.CLASS_NAME, "vis-tooltip")))	
 	print "got url", time.time()-t
 	t= time.time()
 
 	driver.save_screenshot('screen1.png') # save a screenshot to disk
 	print "got screenshot", time.time()-t
 
-	getCommentColumn(BeautifulSoup(driver.page_source, "html.parser"))
+	columnsToJSON(BeautifulSoup(driver.page_source, "html.parser"))
 
 
