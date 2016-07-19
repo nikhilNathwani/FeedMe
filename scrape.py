0e1cc39d070c95cfd90d239345bd9a32f8be3d6e
Nikhil Nathwani <njn27@cornell.edu>
2016-07-19 11:03:01 -0400

    wrote function to format given query and time interval into URL, and then grab screenshot of that URL

--- scrape.py
+++ scrape.py
@@ -5,11 +5,43 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait # available since 2.4.0
 from selenium.webdriver.support import expected_conditions as EC # available since 2.26.0
 
+query="save as"
+startDate="07/01/16"
+endDate="07/18/16"
+
+#returns user's query string and time interval
+def getUserInput():
+	pass
+
+
+#NOTES:
+#--Columns are fixed for now
+#--Platforms are fixed for now
+#dates formatted as YYYY-MM-DD
+def formatQueryURL(query,startDate="",endDate=""):
+	urlBase= 'http://ofeedbackdashboard.cloudapp.net/?#/discover?_g=(refreshInterval:(display:Off,pause:!f,section:0,value:0),'
+	time= 'time:(from:\''+startDate+'T04:00:00.000Z\',mode:absolute,to:\''+endDate+'T03:59:59.999Z\'))'
+	urlMid= '&_a=(columns:!(comment),index:feedback,interval:auto,query:(query_string:(analyze_wildcard:!t,'
+	q= 'query:\'(comment:%22'+query.replace(' ','%20')+'%22)%20AND%20(product:%22desktop%20word%22%20OR%20%22desktop%20powerpoint%22%20OR%20%22desktop%20excel%22)\')),'
+	urlEnd= 'sort:!(submitDate,desc))'
+	return urlBase+time+urlMid+q+urlEnd
 
 
 if __name__ == "__main__":
+	print "starting program"
+	t= time.time()
+
 	driver = webdriver.PhantomJS() # or add to your PATH
 	driver.set_window_size(1024, 1768) # optional
-	driver.get('http://ofeedbackdashboard.cloudapp.net/?#/discover/Detailed-Feedback?_g=(refreshInterval:(display:Off,pause:!f,section:0,value:0),time:(from:now-30d,mode:quick,to:now))&_a=(columns:!(officeBuild,product,comment,isInternal,oSUserLocale,commentURL),filters:!((exists:(field:product),meta:(disabled:!f,index:feedback,key:exists,negate:!f,value:product))),index:feedback,interval:auto,query:(query_string:(analyze_wildcard:!t,query:\'(comment:%22chat%22)%20AND%20(product:%22desktop%20word%22%20OR%20%22desktop%20powerpoint%22%20%20OR%20%22desktop%20excel%22)\')),sort:!(product,asc))')
-	element = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CLASS_NAME, "vis-tooltip")))
-	driver.save_screenshot('screen4.png') # save a screenshot to disk
+	print "shit initialized", time.time()-t
+	t= time.time()
+
+	driver.get(formatQueryURL("Save as",'2016-07-15','2016-07-20'))
+	element = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CLASS_NAME, "vis-tooltip")))	
+	print "got url", time.time()-t
+	t= time.time()
+
+	driver.save_screenshot('screen1.png') # save a screenshot to disk
+	print "got screenshot", time.time()-t
+
+
